ce5903e53c47538290d14e7583d4af76b62fee9d
Nico Virgino <nicovirgino@gmail.com>
2020-03-18 19:14:31 -0500

    working on user c.r.u.d.

---
+++ src/main/java/com/space/project/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.space.project.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+}
---
+++ src/main/java/com/space/project/models/UserRole.java
@@ -0,0 +1,4 @@
+package com.space.project.models;
+
+public class UserRole {
+}
