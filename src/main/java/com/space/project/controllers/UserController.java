adf79b6379a6b6ed913863cbc24786dab8662102
Nico Virgino <nicovirgino@gmail.com>
2020-03-19 19:31:03 -0500

    creating user logic

--- src/main/java/com/space/project/controllers/UserController.java
+++ src/main/java/com/space/project/controllers/UserController.java
@@ -1,10 +1,29 @@
 package com.space.project.controllers;
 
 
+import com.space.project.models.User;
+import com.space.project.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserRepository userDao;
 
+    //create user here
+    public String saveUser(@Valid User user, Errors validation, Model m){
+        String username = user.getUsername();
+        User existingUsername = userDao.findByUsername(username);
+        User existingEmail = userDao.findByEmail(user.getEmail());
+
+
+    }
 }
--- src/main/java/com/space/project/repos/UserRepository.java
+++ src/main/java/com/space/project/repos/UserRepository.java
@@ -4,4 +4,8 @@ import com.space.project.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByUsername(String username);
+    User findByEmail(String email);
+
 }
