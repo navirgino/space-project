72b8b409e0b568102761fa0a1fc6495f6292de22
Nico Virgino <nicovirgino@gmail.com>
2020-03-19 19:23:56 -0500

    setting up repo

---
+++ src/main/java/com/space/project/repos/UserRepository.java
@@ -0,0 +1,7 @@
+package com.space.project.repos;
+
+import com.space.project.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
